8a63340361aca81a00d67fc379d01330d35fa9f3
destradious <venregan@gmail.com>
2020-12-11 12:24:50 -0800

    Clean Reputation of unused booleans

--- src/main/java/net/playavalon/avnrep/data/reputation/Reputation.java
+++ src/main/java/net/playavalon/avnrep/data/reputation/Reputation.java
@@ -17,22 +17,11 @@ public class Reputation {
     private double curve;
     private HashMap<String, Double> sources;
 
-    private boolean hasKillTrigger = false;
-    private boolean hasBreakTrigger = false;
-    private boolean hasPlaceTrigger = false;
-    private boolean hasCraftTrigger = false;
-    private boolean hasMythicTrigger = false;
-    private boolean hasDeathTrigger = false;
-    private boolean hasPlayerLevelTrigger = false;
-    private boolean hasAvnLevelTrigger = false;
-    private boolean hasAvnCraftTrigger = false;
-    private boolean hasAvnGatherTrigger = false;
-
     public Reputation(ConfigurationSection config) {
         if (config == null) return;
         namespace = config.getName().toLowerCase();
         minRep = config.getDouble("FirstLevelCost", 25);
-        maxRep = config.getDouble("LastLevelCost", 10000);
+        maxRep = config.getDouble("LastLevelCost", 5000);
         maxLevel = config.getInt("MaxLevel", 20);
         curve = config.getDouble("RepCurve", -0.5);
 
@@ -42,18 +31,6 @@ public class Reputation {
             String[] pair = source.split(":");
             String trigger = pair[0].toUpperCase();
             sources.put(trigger, Double.parseDouble(pair[1]));
-
-            // Set up booleans for more efficient event catches and early returns
-            if (!hasKillTrigger && trigger.contains("KILL_")) hasKillTrigger = true;
-            if (!hasBreakTrigger && trigger.contains("BREAK_")) hasBreakTrigger = true;
-            if (!hasPlaceTrigger && trigger.contains("PLACE_")) hasPlaceTrigger = true;
-            if (!hasCraftTrigger && trigger.contains("CRAFT_")) hasCraftTrigger = true;
-            if (!hasMythicTrigger && trigger.contains("MYTHIC_")) hasMythicTrigger = true;
-            if (!hasDeathTrigger && trigger.contains("DEATH")) hasDeathTrigger = true;
-            if (!hasPlayerLevelTrigger && trigger.contains("LEVELUP")) hasPlayerLevelTrigger = true;
-            if (!hasAvnLevelTrigger && trigger.contains("CLASS_LEVELUP")) hasAvnLevelTrigger = true;
-            if (!hasAvnCraftTrigger && trigger.contains("AVN_CRAFT")) hasAvnCraftTrigger = true;
-            if (!hasAvnGatherTrigger && trigger.contains("GATHER")) hasAvnGatherTrigger = true;
         }
 
         System.out.println(this);
@@ -85,36 +62,6 @@ public class Reputation {
 
     public boolean hasSource(String source) {
         return sources.containsKey(source.toUpperCase());
-    }
-    public boolean hasKillTrigger() {
-        return hasKillTrigger;
-    }
-    public boolean hasBreakTrigger() {
-        return hasBreakTrigger;
-    }
-    public boolean hasPlaceTrigger() {
-        return hasPlaceTrigger;
-    }
-    public boolean hasCraftTrigger() {
-        return hasCraftTrigger;
-    }
-    public boolean hasMythicTrigger() {
-        return hasMythicTrigger;
-    }
-    public boolean hasDeathTrigger() {
-        return hasDeathTrigger;
-    }
-    public boolean hasPlayerLevelTrigger() {
-        return hasPlayerLevelTrigger;
-    }
-    public boolean hasAvnLevelTrigger() {
-        return hasAvnLevelTrigger;
-    }
-    public boolean hasAvnCraftTrigger() {
-        return hasAvnCraftTrigger;
-    }
-    public boolean hasAvnGatherTrigger() {
-        return hasAvnGatherTrigger;
     }
 
 
